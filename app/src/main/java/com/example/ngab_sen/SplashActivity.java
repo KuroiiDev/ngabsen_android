6eff66fde5c8d9f57184c589edb05ad2cb24e252
Kuroii <101612733+KuroiiDev@users.noreply.github.com>
2023-12-06 13:03:25 +0700

    Splash Screen Jadi

--- app/src/main/java/com/example/ngab_sen/SplashActivity.java
+++ app/src/main/java/com/example/ngab_sen/SplashActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class SplashActivity extends AppCompatActivity {
-
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
