57bb4c75a298d4777ebbe25661b917ce534a9f79
Kuroii <101612733+KuroiiDev@users.noreply.github.com>
2023-12-09 12:50:36 +0700

    Akhirnya Jadi

--- app/src/main/java/com/example/ngab_sen/HomeFragment.java
+++ app/src/main/java/com/example/ngab_sen/HomeFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -27,13 +29,15 @@ public class HomeFragment extends Fragment {
 
     TextView textHalo, textStatus;
     FirebaseFirestore db;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    static String userEmail, userNama, userKelas, userAbsen;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home, null);
 
         textHalo = root.findViewById(R.id.textHalo);
         textStatus = root.findViewById(R.id.statusAbsen);
-        db = FirebaseFirestore.getInstance();
 
         Date getDate = new Date();
         String date = new SimpleDateFormat("dd-MM-YY").format(getDate);
@@ -44,22 +48,47 @@ public class HomeFragment extends Fragment {
         }
         textHalo.setText("Halo, "+MainActivity.getUserNama()+"!");
 
-        DocumentReference dr = db.collection("Absensi").document(date).collection(MainActivity.getUserKelas()).document(MainActivity.getUserAbsen());
-        dr.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()){
-                    DocumentSnapshot doc = task.getResult();
-                    if (doc.exists()) {
-                        textStatus.setText("Anda Sudah Absen Hari Ini");
-                        textStatus.setTextColor(getResources().getColor(R.color.done));
-                    }else {
-                        textStatus.setText("Anda Belum Absen Hari Ini");
-                        textStatus.setTextColor(getResources().getColor(R.color.dont));
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+        if (user != null) {
+            userEmail = user.getEmail();
+            DocumentReference dr = db.collection("Akun").document(user.getEmail());
+            dr.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot doc = task.getResult();
+                        if (doc.exists()) {
+                            userKelas = doc.get("kelas").toString();
+                            userAbsen = doc.get("absen").toString();
+                        }
                     }
                 }
-            }
-        });
+
+            });
+        }
+        try {
+            DocumentReference dr2 = db.collection("Absensi").document(date).collection(userKelas).document(userAbsen);
+            dr2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()){
+                        DocumentSnapshot doc = task.getResult();
+                        if (doc.exists()) {
+                            textStatus.setText("Anda Sudah Absen Hari Ini");
+                            textStatus.setTextColor(getResources().getColor(R.color.done));
+                        }else {
+                            textStatus.setText("Anda Belum Absen Hari Ini");
+                            textStatus.setTextColor(getResources().getColor(R.color.dont));
+                        }
+                    }
+                }
+            });
+        }catch (Exception e){
+            Log.e("Absensi", e.getMessage());
+        }
         return root;
     }
 }
